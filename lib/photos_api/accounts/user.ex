e10bef2b83857c506a4f6b40e6a1440facb58e73
jacek swoboda <jacswob@gmail.com>
2020-01-10 14:23:36 +0100

    create changeset for user

--- lib/photos_api/accounts/user.ex
+++ lib/photos_api/accounts/user.ex
@@ -1,8 +1,16 @@
 defmodule PhotosApi.Accounts.User do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
     field :name, :string
     timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 1, max: 20)
   end
 end
