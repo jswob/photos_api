ed60caff1395b8d8651af416987be6fb1aedc56f
jacek swoboda <jacswob@gmail.com>
2020-01-11 21:14:24 +0100

    create test helper for creating users in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,15 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(PhotosApi.Repo, :manual)
+
+defmodule PhotosApi.TestHelpers do
+  alias PhotosApi.Accounts
+
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into({name: "some name"})
+      |> Accounts.create_user()
+
+    user
+  end
+end
