9a495ef85944aceb89e5d0fd80cc1658ed55da29
jacek swoboda <jacswob@gmail.com>
2020-01-12 15:01:33 +0100

    create functions for menaging user in accounts context

--- lib/photos_api/accounts.ex
+++ lib/photos_api/accounts.ex
@@ -1,3 +1,36 @@
 defmodule PhotosApi.Accounts do
-  # Functinos for working with user schema
+  alias PhotosApi.Repo
+  alias PhotosApi.Accounts.User
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def list_users() do
+    Repo.all(User)
+  end
+
+  def create_user(attrs) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
 end
