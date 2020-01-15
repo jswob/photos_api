d7e52ac75b7cc69f337c2b2176e5e14eeb7cb0e6
jacek swoboda <jacswob@gmail.com>
2020-01-15 12:17:19 +0100

    add bcrypt_elixir and configure it

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :photos_api, PhotosApi.Repo,
 # with webpack to recompile .js and .css sources.
 config :photos_api, PhotosApiWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
--- config/test.exs
+++ config/test.exs
@@ -16,3 +16,5 @@ config :photos_api, PhotosApiWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+config :bcrypt_elixir, :log_rounds, 4
