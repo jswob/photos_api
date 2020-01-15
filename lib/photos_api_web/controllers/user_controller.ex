02b539cd40814e70691bed99849820b6ac9a4691
jacek swoboda <jacswob@gmail.com>
2020-01-15 12:57:26 +0100

    generate user controller with all necessary files and tests

--- lib/photos_api_web/controllers/user_controller.ex
+++ lib/photos_api_web/controllers/user_controller.ex
@@ -1,23 +1,43 @@
 defmodule PhotosApiWeb.UserController do
   use PhotosApiWeb, :controller
 
-  def index(_conn, _params) do
-    # Here api will return all users
+  alias PhotosApi.Accounts
+  alias PhotosApi.Accounts.User
+
+  action_fallback PhotosApiWeb.FallbackController
+
+  def index(conn, _params) do
+    users = Accounts.list_users()
+    render(conn, "index.json", users: users)
   end
 
-  def show(_conn, %{"id" => _id}) do
-    # Here api will return selected user
+  def create(conn, %{"user" => user_params}) do
+    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.user_path(conn, :show, user))
+      |> render("show.json", user: user)
+    end
   end
 
-  def create(_conn, _params) do
-    # Here api will create user
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+    render(conn, "show.json", user: user)
   end
 
-  def update(_conn, %{"id" => _id}) do
-    # Here api will update selected user
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Accounts.get_user!(id)
+
+    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+      render(conn, "show.json", user: user)
+    end
   end
 
-  def delete(_conn, %{"id" => _id}) do
-    # Here api will delete selected user
+  def delete(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+
+    with {:ok, %User{}} <- Accounts.delete_user(user) do
+      send_resp(conn, :no_content, "")
+    end
   end
 end
