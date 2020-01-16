6f25aca397f6adb44afc5b710a0fbf31d1f9ef23
jacek swoboda <jacswob@gmail.com>
2020-01-16 14:51:20 +0100

    update multimedia context

--- lib/photos_api/multimedia.ex
+++ lib/photos_api/multimedia.ex
@@ -1,104 +1,51 @@
 defmodule PhotosApi.Multimedia do
-  @moduledoc """
-  The Multimedia context.
-  """
-
   import Ecto.Query, warn: false
   alias PhotosApi.Repo
 
   alias PhotosApi.Multimedia.Photo
+  alias PhotosApi.Accounts.User
 
-  @doc """
-  Returns the list of photos.
-
-  ## Examples
-
-      iex> list_photos()
-      [%Photo{}, ...]
-
-  """
   def list_photos do
     Repo.all(Photo)
   end
 
-  @doc """
-  Gets a single photo.
+  def list_user_photos(%User{} = user) do
+    Photo
+    |> user_photos_query(user)
+    |> Repo.all()
+  end
 
-  Raises `Ecto.NoResultsError` if the Photo does not exist.
-
-  ## Examples
-
-      iex> get_photo!(123)
-      %Photo{}
-
-      iex> get_photo!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_photo!(id), do: Repo.get!(Photo, id)
 
-  @doc """
-  Creates a photo.
+  @spec get_user_photo!(PhotosApi.Accounts.User.t(), any) :: any
+  def get_user_photo!(%User{} = user, id) do
+    Photo
+    |> user_photos_query(user)
+    |> Repo.get!(id)
+  end
 
-  ## Examples
-
-      iex> create_photo(%{field: value})
-      {:ok, %Photo{}}
-
-      iex> create_photo(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_photo(attrs \\ %{}) do
+  def create_photo(%User{} = user, attrs \\ %{}) do
     %Photo{}
     |> Photo.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a photo.
-
-  ## Examples
-
-      iex> update_photo(photo, %{field: new_value})
-      {:ok, %Photo{}}
-
-      iex> update_photo(photo, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_photo(%Photo{} = photo, attrs) do
     photo
     |> Photo.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Photo.
-
-  ## Examples
-
-      iex> delete_photo(photo)
-      {:ok, %Photo{}}
-
-      iex> delete_photo(photo)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_photo(%Photo{} = photo) do
     Repo.delete(photo)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking photo changes.
-
-  ## Examples
-
-      iex> change_photo(photo)
-      %Ecto.Changeset{source: %Photo{}}
-
-  """
   def change_photo(%Photo{} = photo) do
     Photo.changeset(photo, %{})
+  end
+
+  defp user_photos_query(query, %User{id: user_id}) do
+    from(p in query, where: p.user_id == ^user_id)
   end
 end
